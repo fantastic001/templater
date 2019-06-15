2aa85bd25aa5f321924f2af79dfb048da9d5dabd
Stefan Nožinić <stefan@lugons.org>
2019-06-15 22:13:50 +0200

    Initial commit

---
+++ templater.py
@@ -0,0 +1,38 @@
+
+import sys 
+import os 
+import jinja2
+import os.path
+
+if sys.argv[1] == "--help":
+    print("Usage: templater input_template output param1=value1 param2=value2 ...")
+    exit(0)
+
+if len(sys.argv) < 3:
+    print("Please see templater --help")
+
+path = os.path.abspath(sys.argv[1])
+output = sys.argv[2]
+params = {s.split("=")[0].strip(): s.split("=")[1].strip() for s in sys.argv[3:]}
+
+def generate(path, output, params):
+    for k,v in params.items():
+        output = output.replace("___%s___" % k, v)
+    input_file = open(path)
+    contents = input_file.read()
+    output_file = open(output, "w")
+    output_file.write(jinja2.Template(contents).render(**params))
+
+if os.path.isdir(path):
+    for root, dirs, files in os.walk(path):
+        print(root)
+        my_root = os.path.abspath(root).replace(path, output)
+        for k,v in params.items():
+            my_root = my_root.replace("___%s___" % k , v)
+        # first make path 
+        os.makedirs(my_root, exist_ok=True)
+        for name in files:
+            print("Generating file %s" % os.path.join(my_root, name))
+            generate(os.path.join(root, name), os.path.join(my_root, name), params)
+else:
+    generate(path, output, params)
