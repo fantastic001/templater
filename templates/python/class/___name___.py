68d4f33a295ce20ba742a519d9d906922a57f506
Stefan Nožinić <stefan@lugons.org>
2019-06-29 16:51:47 +0200

    add pass to constructor in python template in case of empty field set

--- templates/python/class/___name___.py
+++ templates/python/class/___name___.py
@@ -1,9 +1,11 @@
 from typing import *
+from . import * 
 class {{ name }}({% if inherit != ""%}{{ inherit }}{%else%}object{%endif%}):
     def __init__(self{%if create_parametrized_constructor%}{% for field, type in fields.items()%}, {{field}}: {{ type }}{%endfor%}{%endif%}):
         {% for field, type  in fields.items() %}
         self.{{ field }} = {% if create_parametrized_constructor%}{{ field }}{%else%}None{%endif%}
         {% endfor %}
+        pass
     {% for method, type in methods.items() %}
     def {{ method }}(self, {% for param, ptype in type.items() %}{% if param != "return"%}{{ param}}: {{ ptype}}{% if not loop.last%}, {%endif%}{%endif%}{% endfor %}) -> {{ type["return"] }}:
         pass
