24b0f64d70821f59515ba00471b3114fdae5eef2
Stefan Nožinić <stefan@lugons.org>
2019-06-29 14:28:55 +0200

    Implement some simple templates

---
+++ templates/python/class/Example.py
@@ -0,0 +1,10 @@
+from typing import *
+class Example(object):
+    def __init__(self, field1: str):
+        
+        self.field1 = field1
+        
+    
+    def method(self, param1: int, param2: str) -> int:
+        pass
+      
---
+++ templates/python/class/___name___.py
@@ -0,0 +1,10 @@
+from typing import *
+class {{ name }}({% if inherit != ""%}{{ inherit }}{%else%}object{%endif%}):
+    def __init__(self{%if create_parametrized_constructor%}{% for field, type in fields.items()%}, {{field}}: {{ type }}{%endfor%}{%endif%}):
+        {% for field, type  in fields.items() %}
+        self.{{ field }} = {% if create_parametrized_constructor%}{{ field }}{%else%}None{%endif%}
+        {% endfor %}
+    {% for method, type in methods.items() %}
+    def {{ method }}(self, {% for param, ptype in type.items() %}{% if param != "return"%}{{ param}}: {{ ptype}}{% if not loop.last%}, {%endif%}{%endif%}{% endfor %}) -> {{ type["return"] }}:
+        pass
+    {%endfor %}  
