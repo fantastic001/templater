e96b069a1703f749643f3d73207a3960545b43fc
Stefan Nožinić <stefan@lugons.org>
2019-08-20 21:25:44 +0200

    Use argument stack for command parsing

--- templater.py
+++ templater.py
@@ -5,27 +5,42 @@ import os
 import jinja2
 import os.path
 import json 
+from ArgumentStack import * 
 
 from src import * 
 from src.sources import * 
 
-if len(sys.argv) < 3 or sys.argv[1] == "--help":
-    print("Usage: templater template output param1=value1 param2=value2 ...")
-    exit(0)
+stack = ArgumentStack("Wrong command") 
+stack.popAll()
+stack.pushCommand("help")
+stack.assignAction(lambda: print(stack.getHelp()), "Get help")
+stack.popAll()
 
-template = sys.argv[1]
-output = sys.argv[2]
-params = {}
-if os.path.isfile(sys.argv[3]):
-    with open(sys.argv[3]) as f:
+
+def do_template(template, output, params):
+    t = DirectoryTemplateSource("./templates").get_template(template)
+    if t is not None:
+        t.generate(output, params)
+    else:
+        print("Error: template not found")
+
+stack.pushCommand("create")
+stack.pushVariable("template")
+stack.pushVariable("output")
+stack.pushCommand("json")
+stack.pushVariable("json_file_path")
+def create_from_template_using_json(template, output, json_file_path):
+    params = {}
+    with open(json_file_path) as f:
         params = json.loads(f.read())
-        x = {s.split("=")[0].strip(): s.split("=")[1].strip() for s in sys.argv[4:]}
-        params = dict(**params, **x)
-else:
-    params = {s.split("=")[0].strip(): s.split("=")[1].strip() for s in sys.argv[3:]}
+    do_template(template, output, params)
+stack.assignAction(create_from_template_using_json, "Generate from template where parameters are in specified json file")    
 
-t = DirectoryTemplateSource("./templates").get_template(template)
-if t is not None:
-    t.generate(output, params)
-else:
-    print("Error: template not found")
+stack.pop()
+stack.pop()
+stack.pushVariable("params_string")
+def create_from_template_using_string(template, output, params_string):
+    params = {s.split("=")[0].strip(): s.split("=")[1].strip() for s in params_string.split(" ")}
+    do_template(template, output, params)
+stack.assignAction(create_from_template_using_string, "Generate from template with parameters specified as string like 'p1=v1 p2=v2'")
+stack.execute(sys.argv)
