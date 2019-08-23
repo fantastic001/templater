e5ce8495d0a1784089b80cfabbf18c2ab9f1d307
Stefan Nožinić <stefan@lugons.org>
2019-08-24 01:40:08 +0200

    Update system test template script

--- templates/sh/cross_system_test/test-___test_name___.sh
+++ templates/sh/cross_system_test/test-___test_name___.sh
@@ -9,29 +9,43 @@ do_test()
 
 check_error() 
 {
-	return 1 # test failed
+	return 0 # test failed
 }
 
 log_error() 
 {
-	echo "TEST FAILED: {{test_name}}" >> $LOG_FILE
+	echo "TEST FAILED: {{test_name}}:$TESTCASE" >> $LOG_FILE
 	{% for variable in variables%} 
 	echo "    | {{variable.upper()}} = ${{variable.upper()}}" >> $LOG_FILE
 	{% endfor %}
 	
 }
 
+do_clean_failed() 
+{
+	echo "$TESTCASE: Clean up"
+}
+
+do_clean_success() 
+{
+	echo "$TESTCASE: Clean up"
+	
+}
+
 echo "Executing test {{ test_name }}"
 echo > $LOG_FILE
 
-{% for testcase in testcases%}
-
-
+{% for testcase in testcases %}
+export TESTCASE="{{ loop.index }}"
 {% for variable in variables %}
 export {{ variable.upper() }}="{{ testcase.get(variable, "") }}"
 {% endfor %}
 do_test
 if check_error; then
 	log_error
+	do_clean_failed
+else
+	do_clean_success
 fi
+
 {% endfor %}
