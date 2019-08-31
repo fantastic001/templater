ff90b756b5e78a1c8a669056291f3d983f262b8a
Stefan Nožinić <stefan@lugons.org>
2019-08-31 19:30:34 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'templater',
+  packages = ['templater', 'templater.sources'],
+  version = '0.1',
+  description = 'Code generation tool based oon given templates',
+  author = 'Stefan Nožinić',
+  author_email = 'stefan@lugons.org',
+  url = 'https://github.com/fantastic001/templater', # use the URL to the github repo
+  download_url = 'https://github.com/fantastic001/templater/tarball/0.1',
+  keywords = ['dev', 'codegen', 'tools'], 
+  package_dir = {'templater': 'src/'},
+  classifiers = [],
+  scripts = ["templater"],
+  install_requires=["ArgumentStack", "jinja2"] # dependencies listed here 
+)
