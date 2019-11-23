c34828305c8fe37c618697038de128c104e50bb7
Stefan Nožinić <stefan@lugons.org>
2019-11-23 21:31:33 +0100

    Make temporary file for specifying template parameters to not mess one in repository

--- src/Template.py
+++ src/Template.py
@@ -4,12 +4,19 @@ import os
 import os.path
 from .TemplateFile import * 
 from .TemplateDirectory import * 
+import datetime
+
+
 class Template:
 
     def __init__(self, path, params_file = "params.tps"):
         self.path = path
         self.params_file = params_file
-
+        self.H = abs(hash(datetime.datetime.now()))
+        path = os.path.join(self.path, self.params_file)
+        with open(path) as f:
+            with open(self.get_params_path(), "w") as tmp:
+                tmp.write(f.read())
     def generate(self, destination_dir_path, params):
         for name in os.listdir(self.path):
             fullpath = os.path.join(self.path, name)
@@ -22,4 +29,11 @@ class Template:
                 template = TemplateFile(fullpath)
                 template.generate(destination_dir_path, params)
     def get_params_path(self):
-        return os.path.join(self.path, self.params_file)
+        import tempfile, shutil, os
+        temp_dir = tempfile.gettempdir()
+        temp_path = os.path.join(temp_dir, "TPS_TEMP_FILE_%d.tps" % self.H)
+        return temp_path
+    
+    def __del__(self):
+        os.remove(self.get_params_path())
+
